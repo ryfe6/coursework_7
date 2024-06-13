dc40c431d0db1c7cfbc354101cee075f86c29914
ryfe <denis.koptelev@gmail.com>
2024-06-13 16:05:36 +0300

    fix coursework

--- main/tests.py
+++ main/tests.py
@@ -23,7 +23,7 @@ class HabitTestCase(APITestCase):
         )
 
     def test_habit_create(self):
-        url = reverse("main:habit_create")
+        url = reverse("habits:habit_create")
         data = {
             "owner": self.user.pk,
             "action": "считать ворон",
@@ -35,7 +35,7 @@ class HabitTestCase(APITestCase):
         self.assertEqual(Habit.objects.all().count(), 2)
 
     def test_habit_list(self):
-        url = reverse("main:habit_list")
+        url = reverse("habits:habit_list")
         response = self.client.get(url)
         data = response.json()
         result = {
@@ -54,7 +54,7 @@ class HabitTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_habit_update(self):
-        url = reverse("main:habit_update", args=(self.habit.pk,))
+        url = reverse("habits:habit_update", args=(self.habit.pk,))
         data = {
             "place": "улица",
             "action": "считать ворон",
@@ -66,7 +66,7 @@ class HabitTestCase(APITestCase):
         self.assertEqual(data.get("place"), "улица")
 
     def test_habit_delete(self):
-        url = reverse("main:habit_delete", args=(self.habit.pk,))
+        url = reverse("habits:habit_delete", args=(self.habit.pk,))
         data = {
             "place": "дом",
             "action": "читать книгу",
@@ -77,7 +77,7 @@ class HabitTestCase(APITestCase):
         self.assertEqual(Habit.objects.all().count(), 0)
 
     def test_habit_time_to_done(self):
-        url = reverse("main:habit_create")
+        url = reverse("habits:habit_create")
         data = {
             "place": "улица",
             "action": "считать ворон",
@@ -88,7 +88,7 @@ class HabitTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_habit_rel_hab_award(self):
-        url = reverse("main:habit_create")
+        url = reverse("habits:habit_create")
         data = {
             "place": "улица",
             "action": "считать ворон",
@@ -100,7 +100,7 @@ class HabitTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_habit_period(self):
-        url = reverse("main:habit_create")
+        url = reverse("habits:habit_create")
         data = {"place": "улица", "считать ворон": "test", "period": 9}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)

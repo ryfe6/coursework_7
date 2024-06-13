32f7c3e988b26d7166d6cdf601b96ce25e2de598
ryfe <denis.koptelev@gmail.com>
2024-06-13 15:22:28 +0300

    fix coursework

--- config/settings.py
+++ config/settings.py
@@ -167,7 +167,7 @@ TG_URL = getenv("TG_URL")
 CELERY_BROKER_URL = "redis://localhost:6379"  # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = getenv("CELERY_RESULT_BACKEND")
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = TIME_ZONE
